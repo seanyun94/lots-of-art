81620225991773efbbea94f04b343c04b448abf0
Sean Yun <sean.yun@me.com>
2018-12-05 23:04:07 -0500

    Complete PullLotURLsFromSale and CacheLotHTMLFromURL tasks in luigi pipeline, change LotDataExtractor class to LotData

--- lot_pipeline/lot_pipeline.py
+++ lot_pipeline/lot_pipeline.py
@@ -11,13 +11,15 @@ class PullLotURLsFromSale(luigi.Task):
 		return []
 
 	def output(self):
+		if not os.path.exists('data/'):
+			os.mkdir('data/')
+
 		sales_url_split = self.sale_url.strip().split('/')
 		sale_name = sale_url_split[3][:-5]
-		return luigi.LocalTarget("data/{}/lot_urls.csv".format(sale_name))
+		return luigi.LocalTarget("data/{}_lot_urls.csv".format(sale_name))
 
 	def run(self):
 		driver = webdriver.Chrome()
-		driver.implicily_wait(30)
 		driver.get(sale_url)
 
 		load_all_link = driver.find_element_by_id('loadAllUpcomingPast')
@@ -26,39 +28,61 @@ class PullLotURLsFromSale(luigi.Task):
 		soup_lots = BeautifulSoup(driver.page_source)
 
 		lots_container = soup_lot.find(id='ResultContainer')
-		for li in lots_container.findall('li'):
-			lot_href = li.find('a', class='cta-image').href
-			info_container = li.find('span', class='infoContainer')
-			lot_no = info_container.p.text.strip().split()[1]
-			print >> out_file, lot_no, lot_href
+		with open(self.output(), 'w') as out_file:
+			for li in lots_container.findall('li'):
+				lot_href = li.find('a', class='cta-image')['href']
+				info_container = li.find('span', class='infoContainer')
+				lot_no = info_container.p.text.strip().split()[1]
+				print >> out_file, lot_no, lot_href
 
 
 class CacheLotHTMLFromUrl(luigi.Task):
 	''' Pull the HTML doc from a url to lot information and cache it
     '''
     sale_url = luigi.Parameter()
-    lot_urls = luigi.Parameter()
 
     def output(self):
-    	sales_url_split = self.sale_url.strip().split('/')
-		sale_name = sale_url_split[3][:-5]
-    	return luigi.LocalTarget("data/html/".format(sale_name))
+    	sales_url_split = self.lot_urls.strip().split('/')
+		self.sale_name = sale_url_split[3].split('.')[0]
+    	return luigi.LocalTarget("data/html/flag_{}".format(sale_name))
 
     def requires():
-    	pass 
+    	return PullLotURLsFromSale(self.sale_url)
 
     def run(self):
-    	pass
+    	driver = webdriver.Chrome()
+    	driver.get(lot_url)
+
+    	if not os.path.exists('data/html/'):
+    		os.mkdir('data/html')
+
+    	elif if not os.path.exists("data/html/{}/".format(self.sale_name)):
+    		os.mkdir("data/html/{}/".format(self.sale_name))
+
+    	with open("data/{}_lot_urls.csv".format(self.sale_name), 'r') as f:
+    		for line in f:
+    			line_split = line.strip().split(',')
+    			soup = BeautifulSoup(line_split[1])
+    			with open("data/html/{0}/lot_{1}.html".format(self.sale_name, line_split[0]), 'w') as html_file:
+    				html_file.write(str(soup))
+
+    	with open(self.output(), 'w') as flag_file:
+        	flag_file.write(' ')
 
 class PullLotDataFromHtml(luigi.Task):
 
-	sale_url = luigi.Parameter()
+	lot_no = luigi.Parameter()
 
 	def output(self):
 		return luigi.LocalTarget("data/{}_lot_info.csv".format(sale_name))
 
 	def requires(self):
-		return [CacheLotHTMLFromUrl(sale_url) for sale_url in 
+		#Get sale names from lot_urls, return Cache task as requirement
+		return [CacheLotHTMLFromUrl(sale_name) for sale_name in sales]
+	def run(self):
+		pass
+
+
 
 
 if __name__ = "__main__":
